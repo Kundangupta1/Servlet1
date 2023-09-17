39c4e03993bd1b8917c5d9a2d2a34ace099d44fc
Kundan Gupta <kundankumar2434@gmail.com>
2023-09-17 09:09:02 +0530

    My initial commit.

---
+++ Sevlet1/src/main/java/com/training/MyServlet.java
@@ -0,0 +1,22 @@
+package com.training;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet
+{
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		PrintWriter out = res.getWriter();
+		out.print("Hello doGet() Method you're so smart"
+				+ "");
+		//backend code
+		System.out.println("Hello doGet() method!");
+	}
+}
